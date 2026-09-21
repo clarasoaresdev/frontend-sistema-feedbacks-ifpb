32473234825d72f1503c9a173530ec9ce9dd911f
clara <clara.soaresdev@gmail.com>
2026-09-20 23:12:50 -0300

    criando tela inicial

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import '/home/clara/Desktop/códigos/front-end-sistema-feedbacks-ifpb/frontend-escuta-ifpb/src/index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
