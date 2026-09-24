4acdd29d3cae7ff4a0b6cad54aaa12f8145499e9
clara <clara.soaresdev@gmail.com>
2026-09-24 13:30:31 -0300

    tela inicial finalizada

--- frontend-escuta-ifpb/src/App.jsx
+++ frontend-escuta-ifpb/src/App.jsx
@@ -8,55 +8,38 @@ function App() {
 
   return (
     <>
-      <nav class="navbar navbar-expand-lg bg-light" data-bs-theme="light">
-        <div class="container-fluid">
-        <a class="navbar-brand" href="#">EscutaIFPB</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarColor03">
-          <ul class="navbar-nav me-auto">
-            <li class="nav-item">
-              <a class="nav-link active" href="#">Home
-                <span class="visually-hidden">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">About</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-              <div class="dropdown-menu">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <a class="dropdown-item" href="#">Something else here</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Separated link</a>
-              </div>
-            </li>
-          </ul>
-          <form class="d-flex">
-            {/*<input class="form-control me-sm-2" type="text" placeholder="Pesquisar" value={texto} onChange={(e) => setTexto(e.target.value)} />*/}
-            <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-          </form>
+    <img src={imagemFundo} className="background" alt="imagem de fundo" />
+    <header id="header">
+      <nav className="navbar navbar-expand-lg bg-light" data-bs-theme="light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">EscutaIFPB</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarColor03">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <a className="nav-link active" href="#">Home
+                  <span className="visually-hidden">(current)</span>
+                </a>
+              </li>           
+            </ul>
+            <form className="d-flex">
+              <button type="button" className="btn btn-secondary">Login</button>
+              <button type="button" className="btn btn-dark">Cadastre-se</button>
+            </form>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </header>
 
     <main>
       <article>
-        <h1 id="aaa">Seja Bem Vindo(a)</h1>
+        <h1 id="titulo">Seja Bem Vindo(a) {'\u{1F4E2}'}</h1>
         <p>
-          O sistema de feedback do IFPB tem como propósito garantir, de forma democrática, um espaço de escuta ativa para todos da comunidade acadêmica  – professores, estudantes, pais ou responsáveis e servidores em geral.
+          O <strong>sistema de escuta do IFPB</strong> tem como propósito garantir, de forma democrática, um espaço de escuta ativa para todos da comunidade acadêmica  – professores, estudantes, pais ou responsáveis e servidores em geral.
           Esse sistema permitirá que membros do IFPB se posicionem acerca de situações vivenciadas na instituição, oferecendo a todos, a oportunidade de expressarem suas perspectivas.
         </p>
-        <img src={imagemFundo} className="background" alt="imagem de fundo" />
       </article>
     </main>
 
--- frontend-escuta-ifpb/src/main.jsx
+++ frontend-escuta-ifpb/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '/home/clara/Desktop/códigos/front-end-sistema-feedbacks-ifpb/frontend-escuta-ifpb/src/index.css'
+import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
