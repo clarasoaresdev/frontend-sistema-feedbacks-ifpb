00c8ac515aa70339c43102b974da0364584eff33
clara <clara.soaresdev@gmail.com>
2026-09-26 11:08:08 -0300

    caquiando com rotas pt.2

--- frontend-escuta-ifpb/src/routes/Cadastro.jsx
+++ frontend-escuta-ifpb/src/routes/Cadastro.jsx
@@ -1,23 +1,45 @@
+import { Link } from 'react-router-dom';
 import '../styles/cadastroStyles.css'
 import 'bootswatch/dist/zephyr/bootstrap.min.css';
 
 const Cadastro = () => {
    return(
-        <>
+        <> 
         <form id="cadastro">
-            <fieldset>
-                <div className="field">
-                    <label for="exampleInputEmail1" className="form-label mt-4">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+            <h1 id="titulo-cad">Cadastro</h1>
+            <fieldset id="fieldset-cad">
+                 <div className="field-cad">
+                    <label for="exampleInputEmail1" className="form-label mt-4">Digite seu CPF ou Matrícula:</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Inserir CPF ou email"></input>
                 </div>
-                <div className="field">
-                    <label for="exampleInputPassword1" className="form-label mt-4">Password</label>
+                <div className="field-cad">
+                    <label for="exampleInputEmail1" className="form-label mt-4">Digite seu nome:</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Inserir nome"></input>
+                </div>
+                  <div className="field-cad">
+                    <label for="exampleInputEmail1" className="form-label mt-4">Cadastre seu email:</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Inserir email"></input>
+                </div>
+                 <div>
+                    <label for="exampleSelect1" class="form-label mt-4" style={{color: 'white'}}>Selecione o tipo de membro</label>
+                    <select class="form-select" id="exampleSelect1">
+                        <option>Estudante</option>
+                        <option>Pai/Responsável</option>
+                        <option>Servidor</option>
+                        <option>Servidor Gestor</option>
+                    </select>
+                </div>
+                <div className="field-cad">
+                    <label for="exampleInputPassword1" className="form-label mt-4">Crie sua senha:</label>
                     <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" autocomplete="off"></input>
                 </div>
-                <div className="field">
-                    <label for="exampleInputPassword1" className="form-label mt-4">Confirm your password</label>
+                <div className="field-cad">
+                    <label for="exampleInputPassword1" className="form-label mt-4">Confirme sua senha:</label>
                     <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" autocomplete="off"></input>
                 </div>
+                <div id="volta-login">
+                    <label>Já possui uma conta?</label><Link to="/Login"><a style={{color: 'white'}}>Faça login</a></Link>
+                 </div>
             </fieldset>
         </form>
     </>
--- frontend-escuta-ifpb/src/routes/Login.jsx
+++ frontend-escuta-ifpb/src/routes/Login.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
-
+import '../styles/loginStyles.css';
+import { Link } from 'react-router-dom';
 const Login = () => {
    return(
-    <div>
-        <h1>Login</h1>
-    </div>
+    <>
+    <form id="login">
+            <h1 id="titulo-login">Login</h1>
+            <fieldset id="fieldset-login">
+                 <div className="field-login">
+                    <label for="exampleInputEmail1" className="form-label mt-4">Digite seu CPF ou Matrícula:</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Inserir CPF ou email"></input>
+                </div>
+                <div className="field-login">
+                    <label for="exampleInputPassword1" className="form-label mt-4">Digite sua senha:</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" autocomplete="off"></input>
+                </div>
+                <div id="ir-cadastro">
+                    <label>Ainda não possui uma conta?</label><Link to="/Cadastro"><a style={{color: 'white'}}>Cadastre-se</a></Link>
+                 </div>
+            </fieldset>
+        </form>
+    </>
    ) ;
 };
 export default Login;
